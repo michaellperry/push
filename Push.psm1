6693ccfc64a137abe9ea3f29bfb5bd3a6460d8a4
Michael L Perry <michael@qedcode.com>
2017-07-15 15:15:34 -0500

    Register a project.

--- Push.psm1
+++ Push.psm1
@@ -1,7 +1,8 @@
 ﻿
 function Register-VSTS {
 param(
-    [string] $TeamName = ""
+    [string] $TeamName = "",
+    [string] $ProjectName = ""
 )
 
     If ((Test-Path "$pwd\VSTSConfig.xml") -eq $False)
@@ -13,19 +14,19 @@ param(
 
     [xml]$Config = Get-Content "$pwd\VSTSConfig.xml"
     $VSTS = $Config.FirstChild
-    $TeamNameAttribute = $VSTS.Attributes["teamName"]
 
+    $TeamNameAttribute = $VSTS.Attributes["teamName"]
     If ($TeamNameAttribute -eq $Null)
     {
         If ($TeamName -eq "")
         {
-            Write-Host "This command registeres a VSTS Team hosted at https://xxxx.visualstudio.com. Please provide the name (the xxxx part)."
+            Write-Host "This command registers a VSTS Team hosted at https://xxxx.visualstudio.com. Please provide the name (the xxxx part)."
             Write-Host
             Write-Host "    Register-VSTeam -TeamName xxxx"
             break
         }
 
-        Write-Host "Registering https://$TeamName.visualstudio.com"
+        Write-Host "Registering https://$TeamName.visualstudio.com."
         $TeamNameAttribute = $Config.CreateAttribute("teamName")
         $TeamNameAttribute.Value = $TeamName
         $VSTS.Attributes.Append($TeamNameAttribute)
@@ -43,21 +44,43 @@ param(
         Write-Host
 
         $PersonalAccessToken = Read-Host "Paste your personal access token from https://$TeamName.visualstudio.com/_details/security/tokens" -AsSecureString
-        Write-Host "Saving personal access token for https://$TeamName.visualstudio.com"
+        Write-Host "Saving personal access token for https://$TeamName.visualstudio.com."
         $Credential = New-StoredCredential -Target "$TeamName.visualstudio.com" -UserName $TeamName -SecurePassword $PersonalAccessToken -Persist Enterprise
     }
 
     $Password = $Credential.GetNetworkCredential().Password
     $BasicAuth = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($Credential.UserName):$($Password)"))
-    $Projects = Invoke-RestMethod `
-        -Headers @{ Authorization = $BasicAuth } `
-        -Uri "https://$TeamName.visualstudio.com/DefaultCollection/_apis/projects?api-version=2.0"
 
-    Write-Host "You have registered https://$TeamName.visualstudio.com. Select a project and run:"
-    Write-Host
-    Write-Host "   Register-VSTS -ProjectName xxxx"
+    $ProjectNameAttribute = $VSTS.Attributes["projectName"]
+    If ($ProjectNameAttribute -eq $Null)
+    {
+        If ($ProjectName -eq "")
+        {
+            $Projects = Invoke-RestMethod `
+                -Headers @{ Authorization = $BasicAuth } `
+                -Uri "https://$TeamName.visualstudio.com/DefaultCollection/_apis/projects?api-version=2.0"
 
-    $Projects.Value | Format-Table Name
+            Write-Host "You have registered https://$TeamName.visualstudio.com. Select a project and run:"
+            Write-Host
+            Write-Host "   Register-VSTS -ProjectName xxxx"
+
+            $Projects.Value | Format-Table Name
+
+            break
+        }
+
+        Write-Host "Registering https://$TeamName.visualstudio.com project $ProjectName."
+        $ProjectNameAttribute = $Config.CreateAttribute("projectName")
+        $ProjectNameAttribute.Value = $ProjectName
+        $VSTS.Attributes.Append($ProjectNameAttribute)
+        $Config.Save("$pwd\VSTSConfig.xml")
+    }
+    Else
+    {
+        $ProjectName = $ProjectNameAttribute.Value
+
+        Write-Output "Team https://$TeamName.visualstudio.com, project $ProjectName is registered."
+    }
 }
 
 Export-ModuleMember -Function Register-VSTS
