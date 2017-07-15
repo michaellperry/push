4b9f365439d8e9e2bec58c71972b7c8b295490a6
Michael L Perry <michael@qedcode.com>
2017-07-15 12:04:56 -0500

    Install a PowerShell module.

---
+++ Install.ps1
@@ -0,0 +1,10 @@
+﻿$PushModule = Get-Module Push
+If ($PushModule -ne $Null)
+{
+    Remove-Module Push
+}
+
+Import-Module .\Push.psm1
+
+Write-Host
+Write-Host "To get started, run Register-VSTeam"
---
+++ Push.psm1
@@ -0,0 +1,17 @@
+﻿function Register-VSTeam {
+param(
+    [string] $Name = ""
+)
+
+    If ($Name -eq "")
+    {
+        Write-Host "This command registeres a VSTS Team hosted at https://xxxx.visualstudio.com. Please provide the name (the xxxx part)."
+        Write-Host
+        Write-Host "    Register-VSTeam xxxx"
+        break
+    }
+
+    Write-Host "Registering https://$Name.visualstudio.com"
+}
+
+Export-ModuleMember -Function Register-VSTeam
